9e2c20670def5c3659b81fe565630f5462760292
ClaudiusMinimus <claudiusminimus@gmail.com>
2020-11-26 07:52:20 -0700

    kubejs added dragon breath

--- minecraft/kubejs/data/sic/kubejs/sic.js
+++ minecraft/kubejs/data/sic/kubejs/sic.js
@@ -4,6 +4,15 @@
 
 // Listen to server recipe event
 events.listen('recipes', function (event) {
+    
+    // Add shaped recipe for chests
+    event.shaped(item.of('minecraft:chest', 4), [
+        'WWW',
+        'W W',
+        'WWW'
+    ], {
+        W: '#minecraft:logs'
+    })
 
     // Add shaped recipe for clay block
      event.shaped(item.of('minecraft:clay', 8), [
@@ -15,17 +24,17 @@ events.listen('recipes', function (event) {
         D: 'minecraft:dirt',
         W: 'minecraft:water_bucket'
     })
-
-    // Add shapeless recipe for sugarcane
-    event.shapeless(item.of('minecraft:sugar_cane', 1), ['minecraft:sugar', 'minecraft:stick'])
-
-    // Add shaped recipe for chests
-     event.shaped(item.of('minecraft:chest', 4), [
-        'WWW',
-        'W W',
-        'WWW'
+    
+    // Add shaped recipe for dragon breath
+    event.shaped(item.of('minecraft:dragon_breath', 8), [
+        'BBB',
+        'BLB',
+        'BBB'
     ], {
-        W: '#minecraft:logs'
-    })
-
+        L: 'minecraft:lava_bucket',
+        B: 'minecraft:glass_bottle'
+    }) 
+    
+    // Add shapeless recipe for sugarcane
+    event.shapeless(item.of('minecraft:sugar_cane', 1), ['minecraft:sugar', 'minecraft:stick'])  
 })
