9c503412218be38c19838df9918e42b6cd30a729
ClaudiusMinimus <claudiusminimus@gmail.com>
2020-12-10 18:08:00 -0700

    copied client changes to server files

--- server/kubejs/data/sic/kubejs/sic.js
+++ server/kubejs/data/sic/kubejs/sic.js
@@ -4,7 +4,7 @@
 
 // Listen to server recipe event
 events.listen('recipes', function (event) {
-    
+
     // Add shaped recipe for chests
     event.shaped(item.of('minecraft:chest', 4), [
         'WWW',
@@ -15,7 +15,7 @@ events.listen('recipes', function (event) {
     })
 
     // Add shaped recipe for clay block
-     event.shaped(item.of('minecraft:clay', 8), [
+    event.shaped(item.of('minecraft:clay', 8), [
         'SDS',
         'DWD',
         'SDS'
@@ -24,17 +24,20 @@ events.listen('recipes', function (event) {
         D: 'minecraft:dirt',
         W: 'minecraft:water_bucket'
     })
-    
+
     // Add shaped recipe for dragon breath
-    event.shaped(item.of('minecraft:dragon_breath', 8), [
+    event.shaped(item.of('minecraft:dragon_breath', 2), [
         'BBB',
         'BLB',
         'BBB'
     ], {
         L: 'minecraft:lava_bucket',
         B: 'minecraft:glass_bottle'
-    }) 
-    
+    })
+
     // Add shapeless recipe for sugarcane
-    event.shapeless(item.of('minecraft:sugar_cane', 1), ['minecraft:sugar', 'minecraft:stick'])  
+    event.shapeless(item.of('minecraft:sugar_cane', 1), ['minecraft:sugar', 'minecraft:stick'])
+
+    // Add shapeless recipe for Survival in Chaos book
+    event.shapeless(item.of('patchouli:guide_book', { 'patchouli:book': 'patchouli:chaos' }), ['minecraft:book', 'minecraft:dirt'])
 })
