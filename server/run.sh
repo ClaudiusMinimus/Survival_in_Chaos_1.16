7bfb66643dae3db111a2413126dc89ba637d7379
ClaudiusMinimus <claudiusminimus@gmail.com>
2021-01-16 15:11:53 -0700

    Update run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cp ~/backupfiles/ftbbackups-common.toml  ~/sic/config
 while true; do
 java -Xms8G -Xmx10G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=35 -XX:G1MaxNewSizePercent=50 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -Dfml.queryResult=confirm -jar forge*.jar nogui
 done
