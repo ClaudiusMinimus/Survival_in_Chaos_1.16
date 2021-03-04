a20f9040a29f7d2c3ed65afa1654f359da6526cf
ClaudiusMinimus <claudiusminimus@gmail.com>
2021-03-03 21:07:14 -0700

    update to 0.22.0

--- sic_mkServer.sh
+++ sic_mkServer.sh
@@ -3,4 +3,4 @@
 #change to acutal build directory if running from main dir
 cd /home/helaman/.local/share/multimc/instances/Survival_in_Chaos_build
 
-/home/helaman/Downloads/Minecraft/modpacks/mkServer/mkServer.sh 'sic-1.16.5-0.21.0' ./server
+/home/helaman/Downloads/Minecraft/modpacks/mkServer/mkServer.sh 'sic-1.16.5-0.22.0' ./server
